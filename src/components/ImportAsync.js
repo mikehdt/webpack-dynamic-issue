9ab52a96058cb1e3a5a6f9a82c518e4aa7f5a1e0
Mike Hopkins <mike@dthought.net>
2018-06-07 10:08:33 +1000

    Small name tidy

--- src/components/ImportAsync.js
+++ src/components/ImportAsync.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const ImportAsyncChild = () => (
+const ImportAsync = () => (
     <div>
         <h2>Asynchronous Import</h2>
         <div>Change me once the project is running! I will update cleanly with 4.9.2 but fail and the page fully reloads with 4.10.2</div>
     </div>
 );
 
-export default ImportAsyncChild;
+export default ImportAsync;
